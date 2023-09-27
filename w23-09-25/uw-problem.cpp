93ac7f0a159f61abd7ac006991f737b6ba5181ce
Dec <112513827+752986@users.noreply.github.com>
2023-09-27 14:40:51 -0600

    Add uw-problem.cpp

---
+++ w23-09-25/uw-problem.cpp
@@ -0,0 +1,29 @@
+#include <fstream>
+#include <istream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void replace_all(istream& input, char toReplace, char replaceWith) {
+	//   this ampersand ^ is very important, otherwise it gives you incredibly unhelpful errors
+	char c;
+	do {
+		c = input.get(); // `get` because stream operators skip whitespace
+		if (c == toReplace) {
+			cout << replaceWith;
+		} else {
+			cout << c;
+		}
+	} while (!input.eof()); // checks if the stream is at end of file
+}
+
+int main() {
+	ifstream input("sample.txt");
+	if (!input.is_open()) {
+		cout << "file not found!" << endl;
+		return 1;
+	}
+	
+	replace_all(input, '.', '!');
+}
